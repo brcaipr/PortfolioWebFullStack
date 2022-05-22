8292087a41276b440874a69049c8d5b2f3808125
brcaipr <braiantorres228@gmail.com>
2022-05-22 17:07:31 -0300

    commit 22-5

--- Front-End-Angular/src/app/app-routing.module.ts
+++ Front-End-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEducacionComponent } from './components/educacion/agregar-educacion/agregar-educacion.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'portfolio',component:PortfolioComponent},
+  {path:'iniciar-sesion',component:IniciarSesionComponent},
+  {path:'',redirectTo:'portfolio',pathMatch:'full'},
+  {path:'agregar-educacion',component:AgregarEducacionComponent}
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
